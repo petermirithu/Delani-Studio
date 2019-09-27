6adc8b2fa6717a55706b9cd838615bfc9107cea6
petermirithu <pyra_m.k@yahoo.com>
2019-09-27 22:26:24 +0300

    add validation to submit button incase of empty data.

--- js/script.js
+++ js/script.js
@@ -11,4 +11,19 @@ $(document).ready(function() {
     $("#img3").toggle();
     $("#prd").toggle();
   });
+  $("#form").submit(function(event) {
+    var name = $("input.name").val();
+    var email = $("input.email").val(); 
+    var message = $("input.message").val();
+
+    var results = name + ", " + "your message has successfully been received.Thank you for visiting us.";
+    if ((name <=[1]) || (email <=[5]) || (message <=[1])) {
+      alert("There is nothing to be sumbited!")
+    }else {
+    alert(results);
+    }
+    
+    $("#story").show();
+    event.preventDefault();
+  });
 });
