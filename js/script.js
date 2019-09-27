bcefa333b8fd088a3ba3ce5031358d1668f9af73
petermirithu <pyra_m.k@yahoo.com>
2019-09-27 11:13:38 +0300

    add click function to development section and development logo.

--- js/script.js
+++ js/script.js
@@ -2,5 +2,13 @@ $(document).ready(function() {
   $("#img1, #dec").click(function() {
     $("#img1").toggle();
     $("#dec").toggle();
-  })
-})
+  });
+  $("#img2, #dev").click(function() {
+    $("#img2").toggle();
+    $("#dev").toggle();
+  });
+  $("#img3, #prd").click(function() {
+    $("#img3").toggle();
+    $("#prd").toggle();
+  });
+});
